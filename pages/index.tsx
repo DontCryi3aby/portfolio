abeb5a852dc265e625aef97e36c6b93f51ee9ab8
thachtryhard <i3oyhp@gmail.com>
2023-10-19 16:01:11 +0700

    Setup default container MUI

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,17 @@
 import { NextPageWithLayout } from '@/models'
 import { Box } from '@mui/material'
+import { Container } from '@mui/material'
 
 const Home: NextPageWithLayout = () => {
-	return <Box>Home Page</Box>
+	return (
+		<Box>
+			<Container maxWidth="sm" sx={{ bgcolor: 'primary.main' }}>
+				SM CONTAINER
+			</Container>
+			<Container sx={{ bgcolor: 'primary.main' }}>MD CONTAINER</Container>
+			<Box>Home Page</Box>
+		</Box>
+	)
 }
 
 export default Home
