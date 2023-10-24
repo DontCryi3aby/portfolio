011895f077c0474be35e3ebce9e327db5adddc7d
thachtryhard <i3oyhp@gmail.com>
2023-10-24 23:15:03 +0700

    Build Footer UI

--- components/common/footer.tsx
+++ components/common/footer.tsx
@@ -1,10 +1,45 @@
-import { Box } from '@mui/material'
+import { Facebook, Instagram, Twitter, LinkedIn } from '@mui/icons-material'
+import { Box, Container, Icon, Typography } from '@mui/material'
 import * as React from 'react'
+
+const SOCIAL_LIST = [
+	{
+		icon: Facebook,
+		url: 'https://www.facebook.com/',
+	},
+	{
+		icon: Instagram,
+		url: 'https://www.instagram.com/',
+	},
+	{
+		icon: Twitter,
+		url: 'https://twitter.com/',
+	},
+	{
+		icon: LinkedIn,
+		url: 'https://www.linkedin.com/',
+	},
+]
 
 export default function Footer() {
 	return (
 		<Box component="footer" py={2} textAlign="center">
-			FOOTER
+			<Container>
+				{SOCIAL_LIST.map((item, index) => (
+					<Box
+						key={index}
+						component="a"
+						href={item.url}
+						target="_blank"
+						rel="noopener noreferrer"
+						p={2}
+					>
+						<item.icon sx={{ fontSize: 48, color: '#21243D' }} />
+					</Box>
+				))}
+			</Container>
+
+			<Typography mt={2}>Copyright ©{new Date().getFullYear()} All rights reserved </Typography>
 		</Box>
 	)
 }
