67bd623d440b8d2c99e98c3fb3549bff51026f31
thachtryhard <i3oyhp@gmail.com>
2023-10-26 10:59:42 +0700

    Add SEO Component and use for Home Page

---
+++ components/common/seo.tsx
@@ -0,0 +1,37 @@
+import * as React from 'react'
+import Head from 'next/head'
+
+export interface SeoData {
+	title: string
+	description: string
+	url: string
+	thumnailUrl: string
+}
+
+export interface SeoProps {
+	data: SeoData
+}
+
+export function Seo({ data }: SeoProps) {
+	const { title, url, thumnailUrl, description } = data
+
+	return (
+		<Head>
+			<title>Portfolio Project</title>
+			<meta name="title" content={title} />
+			<meta name="description" content={description} />
+
+			<meta property="og:type" content="website" />
+			<meta property="og:url" content={url} />
+			<meta property="og:title" content={title} />
+			<meta property="og:description" content={description} />
+			<meta property="og:image" content={thumnailUrl} />
+
+			<meta property="twitter:card" content="summary_large_image" />
+			<meta property="twitter:url" content={url} />
+			<meta property="twitter:title" content={title} />
+			<meta property="twitter:description" content={description} />
+			<meta property="twitter:image" content={thumnailUrl} />
+		</Head>
+	)
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import { Seo } from '@/components/common'
 import { FeaturedWork, HeroSection, RecentPosts } from '@/components/home'
 import { NextPageWithLayout } from '@/models'
 import { Box } from '@mui/material'
@@ -5,6 +6,16 @@ import { Box } from '@mui/material'
 const Home: NextPageWithLayout = () => {
 	return (
 		<Box>
+			<Seo
+				data={{
+					title: 'Portfolio Project',
+					url: 'https://portfolio-nq4ifan3h-dontcryi3aby.vercel.app/',
+					description: 'Make the easiest way for SEO with NextJS',
+					thumnailUrl:
+						'https://res.cloudinary.com/dfuskfien/image/upload/v1698292014/Portfolio/logoNext_m18oww.jpg',
+				}}
+			/>
+
 			<HeroSection />
 			<RecentPosts />
 			<FeaturedWork />
