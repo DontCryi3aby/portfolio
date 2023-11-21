064deb86824be91e7c2bdbd8ff8fe3a09e05ef3f
thachtryhard <i3oyhp@gmail.com>
2023-11-21 20:57:57 +0700

    Add pagination Work Page

--- pages/works/index.tsx
+++ pages/works/index.tsx
@@ -1,11 +1,24 @@
 import { WorkList } from '@/components/work'
 import { useWorkList } from '@/hooks'
-import { Box, Container, Typography } from '@mui/material'
+import { ListParams } from '@/models'
+import { Box, Container, Pagination, Stack, Typography } from '@mui/material'
+import { useState } from 'react'
 
 export interface WorksPageProps {}
 
 export default function WorksPage(props: WorksPageProps) {
-	const { data, isLoading } = useWorkList({ params: { _page: 1, _limit: 3 } })
+	const [filters, setFilters] = useState<Partial<ListParams>>({ _page: 1, _limit: 3 })
+	const { data, isLoading } = useWorkList({ params: filters })
+
+	const { _page, _limit, _totalRows } = data?.pagination || {}
+	const totalPages = !!_totalRows ? Math.ceil(_totalRows / _limit) : 0
+
+	const handlePageChange = (event: React.ChangeEvent<unknown>, value: number) => {
+		setFilters((prev) => ({
+			...prev,
+			_page: value,
+		}))
+	}
 
 	return (
 		<Box>
@@ -15,6 +28,12 @@ export default function WorksPage(props: WorksPageProps) {
 				</Typography>
 
 				<WorkList workList={data?.data || []} loading={isLoading} />
+
+				{totalPages > 1 && (
+					<Stack alignItems="center">
+						<Pagination count={totalPages} page={_page} onChange={handlePageChange} />
+					</Stack>
+				)}
 			</Container>
 		</Box>
 	)
